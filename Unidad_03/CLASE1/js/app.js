aa6957183e0bb0bae9cdf5d31f73e962f17c677e
José Carlos Doblas Mejías <jdobmej0511@cpifpalanturing.es>
2026-01-12 14:47:18 +0100

    sd

--- Unidad_03/CLASE1/js/app.js
+++ Unidad_03/CLASE1/js/app.js
@@ -1,5 +1,35 @@
-"user strict";
+"use strict";
 
 // Variables: let y const
 let edad = 18;
-console.log("Edad: ", edad);
+console.log("Edad:", edad);
+// console.log(edad. constructor.name);
+edad = 27;
+console.log("Nueva edad:" + edad);
+
+const centro = "CPIFP Alan Turing";
+console.log("Centro:", centro);
+// centro = "CPIFP Otr log( ... data: any[]): void
+// console.log("Nuevo
+
+let puntos = 0;
+puntos = puntos + 10;
+console.log("Puntos =", puntos);
+
+// Tipos primitivos y opercaiones básicas
+const a = 10;
+const b = 3;
+
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplicación:", a * b);
+console.log("División:", a / b);
+console.log("Parte entera:", Math.trunc(a / b));
+console.log("Resto (módulo):", a % b);
+console.log("Potenciación:", a ** b);
+
+// Tipos primitivos y manejo de String
+const nombre = "Compi";
+console.log("Hola " + nombre);
+console.log(`Hola ${nombre}`);
+ 
